3aeea0e766d910962c00474bf17037f5897e6231
samres <yososyam4@gmail.com>
2025-04-15 00:53:25 -0400

    add: view show

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 
+def leerArchivo(archivo):
+    data =[];
+    with open(archivo) as f:
+        for x in f:
+            data.append(json.loads(x))
+        a=open("salvada",'w')
+        a.write(str(data))
+        return data
 
 @app.route('/')
 def index():
@@ -15,7 +24,8 @@ def escaneoView():
 @app.route('/escaneo/dominio')#, methods =['POST'])
         
 def escaneoDominio():
-    return render_template('escaneo/show.html')
+    data=leerArchivo('save.json')
+    return render_template('escaneo/show.html',d=data,tam=len(data))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
